b3e02c4519da09611949aa0639532ec6a5ca2357
yafiethaile <120148011+yafiethaile@users.noreply.github.com>
2023-10-06 15:21:03 -0600

    .

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import StudentInfo from './studentInfo'
-import Week2Page from './week 2/w2page';
-import { Link } from 'next/link';
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,6 +8,7 @@ export default function Home() {
       <h1>CPRG 306 Web Development 2 Assignments</h1>
 
       <StudentInfo/>
+      <Link href="/app/week2/w2page">Week 2</Link>
       
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
--- app/week2/w2page.js
+++ app/week2/w2page.js
@@ -1,13 +1,13 @@
 import React from "react";
 import StudentInfo from "../studentInfo";
 
-
 export default function Week2Page() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>My Shopping List</h1>
   
-        <StudentInfo />
+        <StudentInfo/>
       </main>
+
     );
   };
